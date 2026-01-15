cb07f4c7e7d7ad472fbfe94ef5b3c30b268b9124
yada <yada.sin@thailife.com>
2026-01-16 00:38:46 +0700

    result page

---
+++ src/type/colorSets.ts
@@ -0,0 +1,122 @@
+export const COLOR_SETS = {
+  summer: {
+    recommended: [
+      "#F66CA4",
+      "#B25EBA",
+      "#6E77D4",
+      "#19A4D6",
+      "#1294FA",
+
+      "#F8B7C7",
+      "#FBB8B2",
+      "#FCF397",
+      "#9CE1CF",
+      "#BBBFEF",
+
+      "#DBBCC1",
+      "#E5C7C5",
+      "#E3D9B5",
+      "#B1D0D3",
+      "#AAAFC5",
+    ],
+    makeup: ["#F8C9CC", "#F69BC0", "#E7B6F3", "#D3426F"],
+    fortune: {
+      career: [
+        { name: "Blue", color: "#0BA4DB" },
+        { name: "Grey", color: "#E3E6EB" },
+        { name: "Navy", color: "#071D49" },
+      ],
+      love: [
+        { name: "Pink", color: "#FFC3BD" },
+        { name: "Purple", color: "#BFC5FF" },
+      ],
+      wealth: [
+        { name: "Green", color: "#B8EAD6" },
+        { name: "Grey", color: "#AEB3C2" },
+      ],
+    },
+  },
+
+  spring: {
+    recommended: [
+      "#F66CA4",
+      "#B25EBA",
+      "#6E77D4",
+      "#19A4D6",
+      "#1294FA",
+
+      "#F8B7C7",
+      "#FBB8B2",
+      "#FCF397",
+      "#9CE1CF",
+      "#BBBFEF",
+
+      "#DBBCC1",
+      "#E5C7C5",
+      "#E3D9B5",
+      "#B1D0D3",
+      "#AAAFC5",
+    ],
+    makeup: ["#FFAFCC", "#FFD6A5", "#FDFFB6", "#FFC8DD"],
+    fortune: {
+      career: [{ name: "Yellow", color: "#FFD166" }],
+      love: [{ name: "Coral", color: "#FF7A5A" }],
+      wealth: [{ name: "Green", color: "#90DBA1" }],
+    },
+  },
+
+  autumn: {
+    recommended: [
+      "#F66CA4",
+      "#B25EBA",
+      "#6E77D4",
+      "#19A4D6",
+      "#1294FA",
+
+      "#F8B7C7",
+      "#FBB8B2",
+      "#FCF397",
+      "#9CE1CF",
+      "#BBBFEF",
+
+      "#DBBCC1",
+      "#E5C7C5",
+      "#E3D9B5",
+      "#B1D0D3",
+      "#AAAFC5",
+    ],
+    makeup: ["#9C6644", "#B08968", "#CB997E", "#6F1D1B"],
+    fortune: {
+      career: [{ name: "Brown", color: "#7F5539" }],
+      love: [{ name: "Wine", color: "#6F1D1B" }],
+      wealth: [{ name: "Gold", color: "#C9A227" }],
+    },
+  },
+  winter: {
+    recommended: [
+      "#F66CA4",
+      "#B25EBA",
+      "#6E77D4",
+      "#19A4D6",
+      "#1294FA",
+
+      "#F8B7C7",
+      "#FBB8B2",
+      "#FCF397",
+      "#9CE1CF",
+      "#BBBFEF",
+
+      "#DBBCC1",
+      "#E5C7C5",
+      "#E3D9B5",
+      "#B1D0D3",
+      "#AAAFC5",
+    ],
+    makeup: ["#9C6644", "#B08968", "#CB997E", "#6F1D1B"],
+    fortune: {
+      career: [{ name: "Brown", color: "#7F5539" }],
+      love: [{ name: "Wine", color: "#6F1D1B" }],
+      wealth: [{ name: "Gold", color: "#C9A227" }],
+    },
+  },
+};
